e91f9a2c86a526082a70a8fe00c66d3f10ddd80c
Hendi Santika <hendisantika@gmail.com>
2020-02-26 06:30:03 +0700

    #6 - Add func in CircuitBreakerController

--- src/main/java/com/hendisantika/springbootresilience4jsample/circuitbreaker/CircuitBreakerController.java
+++ src/main/java/com/hendisantika/springbootresilience4jsample/circuitbreaker/CircuitBreakerController.java
@@ -1,8 +1,12 @@
 package com.hendisantika.springbootresilience4jsample.circuitbreaker;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerOpenException;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.vavr.control.Try;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,5 +28,12 @@ public class CircuitBreakerController {
     public CircuitBreakerController(CircuitBreakerRegistry registry, CircuitBreakerService service) {
         this.circuitBreaker = registry.circuitBreaker("circuitA");
         this.service = service;
+    }
+
+    @GetMapping("/func")
+    public String func(@RequestParam(required = false) String str) {
+        return Try.ofSupplier(CircuitBreaker.decorateSupplier(circuitBreaker, () -> service.func(str)))
+                .recover(CircuitBreakerOpenException.class, "Circuit is Open!!")
+                .recover(RuntimeException.class, "fallback!!").get();
     }
 }
